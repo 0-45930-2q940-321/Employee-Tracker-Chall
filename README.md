0bec91394a1e1d497bfd6a31c4d79c92ce3bbbbf
906284 <michaelcounseller@gmail.com>
2022-01-23 23:08:30 -0600

    fixed indentions for update-employee.js file, added README.md with information.

---
+++ README.md
@@ -0,0 +1,40 @@
+# Employee-Tracker-Chall
+## License
+![MIT license](https://img.shields.io/badge/License-MIT-yellow.svg)
+
+## Description 
+
+The CLI will let you connect to a database that contains departments, role and employee tables with designated information for each.
+
+## Table of Contents
+
+  - [License](#license)
+  - [Description](#description)
+  - [Table of Contents](#table-of-contents)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [Contact](#contact)
+
+## Installation
+
+If you have node installed, then in the command line just type 'node index'. If you do not have node installed, install it before using the command line.
+
+## Usage
+
+Just type 'node index' in the CLI.
+
+## Credits
+
+If I miss any contributions/credits please feel free to contact me.
+
+---
+
+## Contact
+
+Please contact me if you have any questions.
+
+[Github](https://github.com/94r0372189547389)
+
+Email: michaelcounseller@gmail.com
+
--- cases/update-employee.js
+++ cases/update-employee.js
@@ -3,49 +3,49 @@ const db = require("../db/connect");
 
 updateEmployee = (sql, params) => {
     inquirer
-    .prompt([
-        {
-            type: "number",
-            name: "employeeId",
-            message: "Please enter the ID of the employee you'd like to update: ",
-            validate: employeeIdInput => {
-                if (employeeIdInput) {
-                    return true;
-                } else {
-                    console.log('The ID you entered does NOT EXIST!');
-                    return false;
+        .prompt([
+            {
+                type: "number",
+                name: "employeeId",
+                message: "Please enter the ID of the employee you'd like to update: ",
+                validate: employeeIdInput => {
+                    if (employeeIdInput) {
+                        return true;
+                    } else {
+                        console.log('The ID you entered does NOT EXIST!');
+                        return false;
+                    }
+                }
+            },
+            {
+                type: "number",
+                name: "newEmployeeRole",
+                message: "Please enter the ID of the new role for the employee: ",
+                validate: newEmployeeRoleInput => {
+                    if (!isNaN(newEmployeeRoleInput) && newEmployeeRoleInput.length <= 4) {
+                        return true
+
+                    } else {
+                        console.log('EMPLOYEE ID NEEDS TO BE A NUMBER AND CHARACTER AMOUNT LESS THAN OR EQUAL TO 4')
+                    }
                 }
             }
-        },
-        {
-            type: "number",
-            name: "newEmployeeRole",
-            message: "Please enter the ID of the new role for the employee: ",
-            validate: newEmployeeRoleInput => {
-                if (!isNaN(newEmployeeRoleInput) && newEmployeeRoleInput.length <= 4) {
-                    return true
+        ])
+        .then(data => {
+            sql = `UPDATE employees SET role_id = ? WHERE id =?`;
+            params = [data.newEmployeeRole, data.employeeId];
+
+            db.query(sql, params, (err, rows) => {
+                if (err) {
+                    console.log(err);
 
                 } else {
-                    console.log('EMPLOYEE ID NEEDS TO BE A NUMBER AND CHARACTER AMOUNT LESS THAN OR EQUAL TO 4')
+                    console.log(`EMPLOYEE: ${employeeId} , ROLE has been CHANGED TO ${newEmployeeRole}`);
+
+                    continueCheck();
                 }
-            }
-        }
-    ])
-    .then(data => {
-        sql = `UPDATE employees SET role_id = ? WHERE id =?`;
-        params = [data.newEmployeeRole, data.employeeId];
-
-        db.query(sql, params, (err, rows) => {
-            if (err) {
-                console.log(err);
-
-            } else {
-                console.log(`EMPLOYEE: ${employeeId} , ROLE has been CHANGED TO ${newEmployeeRole}`);
-
-                continueCheck();
-            }
+            });
         });
-    });
 };
 
 module.exports = updateEmployee;
